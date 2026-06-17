3dfe34986ced037ce8a9330367d33b4092fc2dcd
FattahAhmadSabri <fassabri1@gmail.com>
2026-06-18 00:17:30 +0530

    final

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
+  if (req.url === "/") {
     res.setHeader("content-type", "text/html");
     return res.end(`
-    <form action="/submit" method="POST">
-      <label>Name:</label>
-      <input type="text" name="username">
-      <input type="submit" value="Add">
-    </form>
-  `);
-  }
+        <form action="/message" method="POST">
+        <label>Name:</label>
+        <input type="text" name="username">
+        <button type="submit">Add</button>
+        </form>
+    `);
+  } else {
+    if (req.url === "/message") {
+      res.setHeader("content-type", "text/html");
+      let body = [];
+      req.on("data", (chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+      });
 
-  if (req.url === "/submit" && req.method === "POST") {
-    res.writeHead(302, {
-      Location: "/message",
-    });
-    return res.end();
-  }
+      req.on("end", () => {
+        let buffer = Buffer.concat(body);
 
-  if (req.url === "/message") {
-    return res.end(`<h1>Hello from message!</h1>`);
+        let formData = buffer.toString();
+        console.log(formData);
+        let formValue = formData.split("=")[1];
+        fs.writeFile("formValue.txt", formValue, (err) => {
+          console.log(err);
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          res.end();
+        });
+      });
+    }
+    if (req.url === "/read") {
+      fs.readFile("formValue.txt", (err, data) => {
+        res.end(`<h1>${data.toString()}</h1>`);
+      });
+    }
+   return
   }
 
   res.statusCode = 404;
